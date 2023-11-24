a748b91e28f04419b944aebefaa7fb633435329b
Jovan Milanovic <jovanmilanovic999@gmail.com>
2023-11-24 16:07:47 +0100

    Basic styling and wrapper component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
-import Image from 'next/image'
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Hello</h1>
-  )
+    <MaxWidthWrapper>
+      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+          Your market place for high-quality{" "}
+          <span className="text-blue-600">digital assets</span>.
+        </h1>
+        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+          Welcome to DigitialHippo. Every asset on our platform is verified by
+          our team to ensure our highest quality standards
+        </p>
+        <div className="flex flex-col sm:flex-row gap-4 mt-6">
+          <Link href={"/products"}>Browse Trending</Link>
+        </div>
+      </div>
+    </MaxWidthWrapper>
+  );
 }
